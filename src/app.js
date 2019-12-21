c71477111ff7a56f5c8687a1591c8cfbef9391e6
Dyego Vasconcelos de Albuquerque <dyegovasconcelosdealbuquerque@MacBook-Air-de-Dyego-2.local>
2019-12-21 18:30:13 -0300

    removing module dependence

--- src/app.js
+++ src/app.js
@@ -1,10 +1,8 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const routes = require('./routers');
-var cors = require('cors');
 
 const app = express();
-app.use(cors());
 app.use(bodyParser.json());
 app.use('/', routes);
 
